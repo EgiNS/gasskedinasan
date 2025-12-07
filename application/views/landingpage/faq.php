1491ba912d05e2f22b662aeef0285f3511e70907
muhammadsaman77 <samanmuhammad077@gmail.com>
2025-12-07 16:40:47 +0800

    change data in landing page frequent asl & question

--- application/views/landingpage/faq.php
+++ application/views/landingpage/faq.php
@@ -1,103 +1,192 @@
 <style>
-    
-        .gk-bg-faq {
-            background-color: #1F2A37;
-        }
+  .gk-bg-faq {
+    background-color: #1F2A37;
+  }
 
-        .gk-h3 {
-            font-size: 22px;
-            font-weight: bold;
-            color:  #F9FAFB;
-        }
-        .gk-p {
-            font-weight: bold;
-            font-size: 38px;
-            line-height: 1.5;
-            width: 44%;
-            color:  #F9FAFB;
-            text-align: center;
-            margin: 0 auto;
-            margin-bottom: 60px;
-        }
-        .accordion {
-          width: 75%;
-        }
-        @media (max-width: 992px) {
-            .faq-section {
-                padding: 40px 20px !important;
-            }
-            .gk-p {
-              font-size: 30px;
-              width: 100%;
-            }
-            .accordion {
-              width: 100%;
-            }
-        }
+  .gk-h3 {
+    font-size: 22px;
+    font-weight: bold;
+    color: #F9FAFB;
+  }
+
+  .gk-p {
+    font-weight: bold;
+    font-size: 38px;
+    line-height: 1.5;
+    width: 44%;
+    color: #F9FAFB;
+    text-align: center;
+    margin: 0 auto;
+    margin-bottom: 60px;
+  }
+
+  .accordion {
+    width: 75%;
+  }
+
+  @media (max-width: 992px) {
+    .faq-section {
+      padding: 40px 20px !important;
+    }
+
+    .gk-p {
+      font-size: 30px;
+      width: 100%;
+    }
+
+    .accordion {
+      width: 100%;
+    }
+  }
 </style>
 
 <div id="id-faq-section" class="container-fluid gk-bg-faq faq-section" style="padding: 70px 100px;">
-    <div class=" text-center">
-        <h3 class="gk-h3">
-            Pertanyaan yang Sering Ditanyakan
-        </h3>
+  <div class=" text-center">
+    <h3 class="gk-h3">
+      Pertanyaan yang Sering Ditanyakan
+    </h3>
 
-        <p class="gk-p">
-            Temukan jawaban atas pertanyaan umum 
-seputar program bimbel di sini.
-        </p>
+    <p class="gk-p">
+      Temukan jawaban atas pertanyaan umum
+      seputar program bimbel di sini.
+    </p>
+  </div>
+
+  <div class="accordion" style="margin: 0 auto;" id="accordionExample">
+    <div class="accordion-item">
+      <h2 class="accordion-header">
+        <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+          Apa itu Gasskedinasan?
+        </button>
+      </h2>
+      <div id="collapseOne" class="accordion-collapse collapse show" data-bs-parent="#accordionExample">
+        <div class="accordion-body">
+          Gasskedinasan adalah platform bimbingan dan try out online yang membantu persiapan tes masuk sekolah kedinasan dan CPNS, berbasis sistem CAT dan materi terstruktur.
+        </div>
+      </div>
+    </div>
+    <div class="accordion-item">
+      <h2 class="accordion-header">
+        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+          Siapa saja yang bisa mengikuti program di Gasskedinasan?
+        </button>
+      </h2>
+      <div id="collapseTwo" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+        <div class="accordion-body">
+          Siswa SMA/SMK/MA, mahasiswa, maupun Umum yang ingin mengikuti seleksi sekolah kedinasan atau CPNS.
+        </div>
+      </div>
+    </div>
+    <div class="accordion-item">
+      <h2 class="accordion-header">
+        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+          Apakah Gasskedinasan resmi dan terpercaya?
+        </button>
+      </h2>
+      <div id="collapseThree" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+        <div class="accordion-body">
+          Ya. Gasskedinasan dikelola oleh tutor berpengalaman dan telah digunakan oleh ribuan peserta dari berbagai daerah di Indonesia.
+        </div>
+      </div>
+    </div>
+    <div class="accordion-item">
+      <h2 class="accordion-header">
+        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
+          Program apa saja yang tersedia?
+
+        </button>
+      </h2>
+      <div id="collapseFour" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+        <div class="accordion-body">
+          <ul>
+          <li>Try Out SKD berbasis CAT</li>
+          <li>Bimbel Intensif SKD</li>
+          <li>Grup Belajar</li>
+          <li>Modul belajar Siap Lolos SKD</li>
+          <li>Try Out Matematika STIS</li>
+          <li>Bimbel Intensif Lolos STIS</li>
+          <li>Modul Siap Lolos STIS</li>  
+          </ul>
+        
+        </div>
+      </div>
+    </div>
+    <div class="accordion-item">
+      <h2 class="accordion-header">
+        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFive" aria-expanded="false" aria-controls="collapseFive">
+          Apakah tersedia paket gratis?
+
+        </button>
+      </h2>
+      <div id="collapseFive" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+        <div class="accordion-body">
+          Ya, Gasskedinasan secara berkala menyediakan try out gratis dan event belajar gratis.
+
+
+        </div>
+      </div>
+    </div>
+    <div class="accordion-item">
+      <h2 class="accordion-header">
+        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseSix" aria-expanded="false" aria-controls="collapseSix">
+          Apakah bisa dikerjakan lebih dari sekali ?
+
+        </button>
+      </h2>
+      <div id="collapseSix" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+        <div class="accordion-body">
+         iya, Try Out  dapat dikerjakan berkali kali (Premium)
+
+        </div>
+      </div>
+    </div>
+    <div class="accordion-item">
+      <h2 class="accordion-header">
+        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseSeven" aria-expanded="false" aria-controls="collapseSeven">
+          Apakah tersedia analisis soal dan pembahasan ?
+
+        </button>
+      </h2>
+      <div id="collapseSeven" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+        <div class="accordion-body">
+          Tersedia answer analysis setelah pengerjaan dan pembahasan dapat di download (Premium)
+
+
+        </div>
+      </div>
+    </div>
+    <div class="accordion-item">
+      <h2 class="accordion-header">
+        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseEight" aria-expanded="false" aria-controls="collapseEight">
+          Saya lupa password, apa yang harus dilakukan?
+
+
+        </button>
+      </h2>
+      <div id="collapseEight" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+        <div class="accordion-body">
+          Gunakan fitur Lupa Password atau hubungi admin.
+
+
+        </div>
+      </div>
+    </div>
+        <div class="accordion-item">
+      <h2 class="accordion-header">
+        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseNine" aria-expanded="false" aria-controls="collapseNine">
+          Metode pembayaran apa saja yang tersedia?
+
+        </button>
+      </h2>
+      <div id="collapseNine" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+        <div class="accordion-body">
+          Transfer bank dan e-wallet (sesuai yang tertera di website).
+
+        </div>
+      </div>
     </div>
 
-    <div class="accordion" style="margin: 0 auto;" id="accordionExample">
-  <div class="accordion-item">
-    <h2 class="accordion-header">
-      <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-        Bagaimana cara mendaftar di Gass Education?
-      </button>
-    </h2>
-    <div id="collapseOne" class="accordion-collapse collapse show" data-bs-parent="#accordionExample">
-      <div class="accordion-body">
-        <strong>This is the first item’s accordion body.</strong> It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It’s also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
-      </div>
-    </div>
+
   </div>
-  <div class="accordion-item">
-    <h2 class="accordion-header">
-      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-        Apakah bisa belajar lewat HP?
-      </button>
-    </h2>
-    <div id="collapseTwo" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
-      <div class="accordion-body">
-        <strong>This is the second item’s accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It’s also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
-      </div>
-    </div>
-  </div>
-  <div class="accordion-item">
-    <h2 class="accordion-header">
-      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-        Apakah ada kelas gratis?
-      </button>
-    </h2>
-    <div id="collapseThree" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
-      <div class="accordion-body">
-        <strong>This is the third item’s accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It’s also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
-      </div>
-    </div>
-  </div>
-  <div class="accordion-item">
-    <h2 class="accordion-header">
-      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-        Bagaimana metode pembayarannya?
-      </button>
-    </h2>
-    <div id="collapseThree" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
-      <div class="accordion-body">
-        <strong>This is the forth item’s accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It’s also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
-      </div>
-    </div>
-  </div>
-  
-</div>
-    
+
 </div>
